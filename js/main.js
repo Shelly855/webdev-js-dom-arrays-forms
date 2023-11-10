9c3c28c9ec29af2cbed59509d65300aa8561abfa
Shelly855 <c3011737@hallam.shu.ac.uk>
2023-11-10 10:58:45 +0000

    js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
-document.getElementById('myHeading').innerHTML = "Your Name"; 
+document.getElementById('myHeading').innerHTML = "Your Name";
 document.querySelector('nav ul li').setAttribute('class', 'currentPage');
+
+document.getElementById('myTestEvent').addEventListener('click',
+function(ev) {
+    console.dir(ev);
+})
+
+document.querySelector(".red").addEventListener('click', function(ev){
+    document.querySelector('body').setAttribute('class', "redBack");
+})
+
+document.querySelector(".blue").addEventListener('click', function(ev){
+    document.querySelector('body').setAttribute('class', "blueBack");
+})
+
+document.querySelector(".green").addEventListener('click', function(ev){
+    document.querySelector('body').setAttribute('class', "greenBack");
+})
+
+document.querySelector(".reset").addEventListener('click', function(ev){
+    document.querySelector('body').setAttribute('class', 'resetBack');
+})
+
+let imageAr = ['images/view1.jpg', 'images/view2.jpg', 'images/view3.jpg', 'images/view4.jpg', 'images/view5.jpg', 'images/view6.jpg'];
+document.getElementById('myImages').setAttribute('src', imageAr[2]);
+
