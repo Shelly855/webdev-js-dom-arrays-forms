af1567e18adebb328984b782f4c0df0fac193dd1
Shelly855 <c3011737@hallam.shu.ac.uk>
2023-11-17 10:03:49 +0000

    js

--- js/main.js
+++ js/main.js
@@ -22,10 +22,11 @@ document.querySelector(".reset").addEventListener('click', function(ev){
     document.querySelector('body').setAttribute('class', 'resetBack');
 })
 
-let imageAr = ['images/view1.jpg', 'images/view2.jpg', 'images/view3.jpg', 'images/view4.jpg', 'images/view5.jpg', 'images/view6.jpg'];
+let imageAr = ['images/view1.jpg', 'images/view2.jpg','images/view3.jpg', 'images/view4.jpg', 'images/view5.jpg', 'images/view6.jpg'];
 document.getElementById('myImages').setAttribute('src', imageAr[2]);
 
 function chgImage(){
     console.info('Called');
 }
-setInterval(chgImage, 2000);
+setInterval(chgImage, 4000);
+
